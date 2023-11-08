2912543b800a83de7a49c888db755c92ba9c0795
John Doe <johndoe@email.com>
2023-11-08 11:15:42 +0200

    Exercise 9.1: Registration Plate Equals & HashCode

---
+++ src/com/karel/program/RegistrationPlate.java
@@ -0,0 +1,31 @@
+package com.karel.program;
+
+import java.util.Objects;
+
+public class RegistrationPlate {
+    private final String regCode;
+    private final String country;
+
+    public RegistrationPlate(String regCode, String country) {
+        this.regCode = regCode;
+        this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegistrationPlate that = (RegistrationPlate) o;
+        return Objects.equals(regCode, that.regCode) &&
+               Objects.equals(country, that.country);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(regCode, country);
+    }
+
+    public String toString() {
+        return country + " " + regCode;
+    }
+}
